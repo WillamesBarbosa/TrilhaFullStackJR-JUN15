19b410923f29e7f4b00b6fa94e82c18285696a41
WillamesBarbosa <willamessilva1999@gmail.com>
2024-08-07 20:34:55 -0300

    feat(userController): Create the Index Method
    create the index method responsible for calling the userRepository, waiting for the return, and returning the appropriate response

--- back-end/src/app/controllers/userController.js
+++ back-end/src/app/controllers/userController.js
@@ -2,6 +2,23 @@ const responsesHTTP = require('../../utils/responsesHTTP');
 const UserRepository = require('../repositories/userRepository');
 
 class UserController {
+  async index(request, response) {
+    try {
+      const rows = await UserRepository.findAll();
+
+      // checks if rows.length is 0 returning the correct response if it is true
+      if (rows.length === 0) {
+        response.status(responsesHTTP.NO_CONTENT.status).json(responsesHTTP.NO_CONTENT);
+      }
+
+      return response.json(rows);
+    } catch (error) {
+      return response
+        .status(responsesHTTP.INTERNAL_SERVER_ERROR.status)
+        .json(responsesHTTP.INTERNAL_SERVER_ERROR);
+    }
+  }
+
   async store(request, response) {
     try {
       const {
