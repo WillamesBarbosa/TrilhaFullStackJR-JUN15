290cb815ded3eba9107a4934f631dc78ab460562
WillamesBarbosa <willamessilva1999@gmail.com>
2024-08-06 21:33:27 -0300

    feat: A database connection pool now exists
    There is now a pool of connections to the database, now better optimized connections to the database

--- back-end/src/database/config.js
+++ back-end/src/database/config.js
@@ -1,39 +1,17 @@
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
+const { Sequelize } = require('sequelize');
 const path = require('path');
 
-// function that creates the database file and tables
-async function database() {
-  try {
-    const db = await open({
-      filename: path.resolve(__dirname, './database.db'),
-      driver: sqlite3.Database,
-    });
-
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS users(
-          id UUID PRIMARY KEY,
-          name TEXT NOT NULL,
-          email TEXT UNIQUE NOT NULL,
-          password TEXT
-          role TEXT DEFAULT 'user'
-          )
-      `);
-
-    await db.exec(`CREATE TABLE IF NOT EXISTS projects (
-        id UUID PRIMARY KEY,
-        title TEXT NOT NULL,
-        description TEXT,
-        user_id UUID NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users (id)
-      )
-    `);
-
-    return db;
-  } catch (error) {
-    console.log(error);
-    throw new Error('Houve um problema ao conectar-se ao banco de dados.');
-  }
-}
+// Connection pool settings
+/* eslint-disable */
+  const database = new Sequelize({
+    dialect: 'sqlite',
+    storage: path.resolve(__dirname, './database.db'),
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  });
 
 module.exports = database;
