a72510abf248397eec7951fdd0c1b985bdeffb07
WillamesBarbosa <willamessilva1999@gmail.com>
2024-08-07 20:39:02 -0300

    feat(userRepository): Create the findAll method
    Create the method responsible for performing a query and returning all users

--- back-end/src/app/repositories/userRepository.js
+++ back-end/src/app/repositories/userRepository.js
@@ -1,6 +1,12 @@
 const User = require('../../models/userModel');
 
 class UserRepository {
+  async findAll() {
+    const users = await User.findAll();
+
+    return users;
+  }
+
   async create(full_name, username, email, password) {
     const row = await User.create(
       {
